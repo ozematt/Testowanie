a4f11154e8c24b5c84a9bbdaeb8a7be8f7810341
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-31 11:42:00 +0200

    added test to multiplay function

--- 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.spec.js
+++ 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.spec.js
@@ -1,2 +1,8 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, test } from "vitest";
 import { multiply, isAdult, getUsername } from "./functions";
+
+discribe("multiplay two numbers", () => {
+  test("2 * 2 shoul return 4", () => {
+    expect(multiply(2, 2)).toBe(4);
+  });
+});
