7f986b79642dadee2893d679746c88bd84734e71
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-31 11:49:01 +0200

    added test for isAdult function

--- 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.spec.js
+++ 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.spec.js
@@ -1,8 +1,19 @@
 import { describe, it, expect, test } from "vitest";
 import { multiply, isAdult, getUsername } from "./functions";
 
-discribe("multiplay two numbers", () => {
+discribe("test multiplay function", () => {
   test("2 * 2 shoul return 4", () => {
     expect(multiply(2, 2)).toBe(4);
+  });
+});
+
+discribe("test isAdult function", () => {
+  test.each([
+    [18, true],
+    [20, true],
+    [17, false],
+    [16, false],
+  ])("isAdult for %s returns %s", (number, expected) => {
+    expect(isAdult(number)).toBe(expected);
   });
 });
