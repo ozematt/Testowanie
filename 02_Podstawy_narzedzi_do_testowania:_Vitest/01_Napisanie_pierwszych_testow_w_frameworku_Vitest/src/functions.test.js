d9d144659a879032dc8c1db2636780d766ce5051
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-31 12:42:28 +0200

    added two tests for getUsername function

--- 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.test.js
+++ 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.test.js
@@ -7,13 +7,29 @@ describe("test multiplay function", () => {
   });
 });
 
-// describe("test isAdult function", () => {
-//   test.each([
-//     [18, true],
-//     [20, true],
-//     [17, false],
-//     [16, false],
-//   ])("isAdult for %s returns %s", (number, expected) => {
-//     expect(isAdult(number)).toBe(expected);
-//   });
-// });
+describe("test isAdult function", () => {
+  test.each([
+    [18, true],
+    [20, true],
+    [17, false],
+    [16, false],
+  ])("isAdult for %s returns %s", (number, expected) => {
+    expect(isAdult(number)).toBe(expected);
+  });
+});
+
+describe("test getUsername function", () => {
+  test("should resolve with user data when userId is 1", async () => {
+    const userId = 1;
+    const expected = { id: 1, name: "John Doe" };
+
+    const result = await getUsername(userId);
+    expect(result).toEqual(expected);
+  });
+  test("should reject with error message when userId is not 1", async () => {
+    const userId = 2;
+    const expectedError = new Error("User not found");
+
+    await expect(getUsername(userId)).rejects.toEqual(expectedError);
+  });
+});
