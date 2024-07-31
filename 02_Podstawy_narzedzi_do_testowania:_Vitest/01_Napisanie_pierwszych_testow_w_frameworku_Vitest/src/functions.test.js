c848b91e0a789c12b6f929efb8b1dc174b5c35b6
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-31 16:23:07 +0200

    added describe

--- 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.test.js
+++ 02_Podstawy_narzedzi_do_testowania:_Vitest/01_Napisanie_pierwszych_testow_w_frameworku_Vitest/src/functions.test.js
@@ -1,34 +1,35 @@
 import { describe, it, expect, test } from "vitest";
 import { multiply, isAdult, getUsername } from "./functions";
-
-describe("test multiplay function", () => {
-  test("2 * 2 should return 4", () => {
-    expect(multiply(2, 2)).toBe(4);
+describe("test functions", () => {
+  describe("test multiplay function", () => {
+    test("2 * 2 should return 4", () => {
+      expect(multiply(2, 2)).toBe(4);
+    });
   });
-});
 
-describe("test isAdult function", () => {
-  test.each([
-    [18, true],
-    [20, true],
-    [17, false],
-    [16, false],
-  ])("isAdult for %s returns %s", (number, expected) => {
-    expect(isAdult(number)).toBe(expected);
+  describe("test isAdult function", () => {
+    test.each([
+      [18, true],
+      [20, true],
+      [17, false],
+      [16, false],
+    ])("isAdult for %s returns %s", (number, expected) => {
+      expect(isAdult(number)).toBe(expected);
+    });
   });
-});
 
-describe("test getUsername function", () => {
-  test("should resolve with user data when userId is 1", async () => {
-    const userId = 1;
-    const expected = { id: 1, name: "John Doe" };
+  describe("test getUsername function", () => {
+    test("should resolve with user data when userId is 1", async () => {
+      const userId = 1;
+      const expected = { id: 1, name: "John Doe" };
 
-    await expect(getUsername(userId)).resolves.toEqual(expected);
-  });
-  test("should reject with error message when userId is not 1", async () => {
-    const userId = 2;
-    const expectedError = new Error("User not found");
+      await expect(getUsername(userId)).resolves.toEqual(expected);
+    });
+    test("should reject with error message when userId is not 1", async () => {
+      const userId = 2;
+      const expectedError = new Error("User not found");
 
-    await expect(getUsername(userId)).rejects.toEqual(expectedError);
+      await expect(getUsername(userId)).rejects.toEqual(expectedError);
+    });
   });
 });
