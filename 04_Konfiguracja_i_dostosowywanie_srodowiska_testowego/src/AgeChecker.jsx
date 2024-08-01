aa6730547aea04a4c43d97d1adaaaefd3ccc7cf6
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-01 16:07:55 +0200

    added tetst

--- 04_Konfiguracja_i_dostosowywanie_srodowiska_testowego/src/AgeChecker.jsx
+++ 04_Konfiguracja_i_dostosowywanie_srodowiska_testowego/src/AgeChecker.jsx
@@ -1,3 +1,65 @@
-export const Example = () => {
-  return <div>Example</div>;
+import { useEffect, useState } from "react";
+
+export const LABELS = {
+  CHECK: "Check",
+  LOADING: "Loading...",
+  TYPE_YOUR_AGE: "Type your age...",
+  YOU_ARE_ADULT: "You are adult.",
+  YOU_ARE_MINOR: "You are minor.",
 };
+
+export const AgeChecker = () => {
+  const [age, setAge] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [value, setValue] = useState("");
+
+  const handleBlur = () => {
+    setAge(Number(value));
+  };
+
+  const handleChange = (event) => setValue(event.target.value);
+
+  const handleClick = () => setIsLoading(true);
+
+  useEffect(() => {
+    if (!isLoading) {
+      return;
+    }
+
+    setIsLoading(true);
+    const timer = setTimeout(() => {
+      const newMessage =
+        age >= 18 ? LABELS.YOU_ARE_ADULT : LABELS.YOU_ARE_MINOR;
+
+      setMessage(newMessage);
+      setIsLoading(false);
+    }, 250);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [age, isLoading]);
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={age}
+        placeholder={LABELS.TYPE_YOUR_AGE}
+        onBlur={handleBlur}
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>{LABELS.CHECK}</button>
+      <Label isLoading={isLoading} label={message} />
+    </div>
+  );
+};
+
+function Label({ isLoading, label }) {
+  if (isLoading) {
+    return <p role="status">{LABELS.LOADING}</p>;
+  }
+
+  return <p>{label}</p>;
+}
--- 04_Konfiguracja_i_dostosowywanie_srodowiska_testowego/src/AgeChecker.spec.jsx
+++ 04_Konfiguracja_i_dostosowywanie_srodowiska_testowego/src/AgeChecker.spec.jsx
@@ -1,4 +1,16 @@
 import { AgeChecker } from "./AgeChecker";
 import { render, screen } from "@testing-library/react";
 
-describe("test <AgeCkecker />", () => {});
+describe("test <AgeCkecker />", () => {
+  test("not displays 'Loading...' after render ", () => {
+    render(<AgeChecker />);
+
+    expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+  });
+
+  test("not displays 'You are adult.' after render ", () => {
+    render(<AgeChecker />);
+
+    expect(screen.queryByText("You are adult.")).not.toBeInTheDocument();
+  });
+});
