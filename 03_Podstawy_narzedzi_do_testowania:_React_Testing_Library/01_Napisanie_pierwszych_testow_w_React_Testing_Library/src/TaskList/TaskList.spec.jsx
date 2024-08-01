de95afbd155d9e8c99a7a2b5308b63c2695b3f75
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-01 09:21:48 +0200

    added test loading message chceck

--- 03_Podstawy_narzedzi_do_testowania:_React_Testing_Library/01_Napisanie_pierwszych_testow_w_React_Testing_Library/src/TaskList/TaskList.spec.jsx
+++ 03_Podstawy_narzedzi_do_testowania:_React_Testing_Library/01_Napisanie_pierwszych_testow_w_React_Testing_Library/src/TaskList/TaskList.spec.jsx
@@ -3,4 +3,9 @@ import { render, screen } from "@testing-library/react";
 
 import { TaskList } from "./TaskList";
 
-describe("<TaskList />", () => {});
+describe("<TaskList />", () => {
+  test("renders loading", () => {
+    render(<TaskList />);
+    expect(screen.getByText("Data is loading...")).toBeInTheDocument();
+  });
+});
