ffd0aa0b45797a720d93202879c10d0aff1380e8
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-01 16:10:59 +0200

    refactor

--- 04_Konfiguracja_i_dostosowywanie_srodowiska_testowego/src/AgeChecker.spec.jsx
+++ 04_Konfiguracja_i_dostosowywanie_srodowiska_testowego/src/AgeChecker.spec.jsx
@@ -1,22 +1,24 @@
 import { AgeChecker } from "./AgeChecker";
 import { render, screen } from "@testing-library/react";
 
-describe("test <AgeCkecker />", () => {
-  test("not displays 'Loading...' after render ", () => {
-    render(<AgeChecker />);
+describe("test <AgeChecker />", () => {
+  describe("tests after component <AgeChecker /> render", () => {
+    test("not displays 'Loading...' after render ", () => {
+      render(<AgeChecker />);
 
-    expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
-  });
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    });
 
-  test("not displays 'You are adult.' after render ", () => {
-    render(<AgeChecker />);
+    test("not displays 'You are adult.' after render ", () => {
+      render(<AgeChecker />);
 
-    expect(screen.queryByText("You are adult.")).not.toBeInTheDocument();
-  });
+      expect(screen.queryByText("You are adult.")).not.toBeInTheDocument();
+    });
 
-  test("not displays 'You are minor.' after render ", () => {
-    render(<AgeChecker />);
+    test("not displays 'You are minor.' after render ", () => {
+      render(<AgeChecker />);
 
-    expect(screen.queryByText("You are minor.")).not.toBeInTheDocument();
+      expect(screen.queryByText("You are minor.")).not.toBeInTheDocument();
+    });
   });
 });
