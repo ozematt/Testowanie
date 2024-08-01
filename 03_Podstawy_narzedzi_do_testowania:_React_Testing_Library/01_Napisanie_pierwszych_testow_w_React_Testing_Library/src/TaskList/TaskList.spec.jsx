f832103f23b44c9511c66a4e6ae045db504c331a
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-01 09:34:17 +0200

    added test for empty tasks list

--- 03_Podstawy_narzedzi_do_testowania:_React_Testing_Library/01_Napisanie_pierwszych_testow_w_React_Testing_Library/src/TaskList/TaskList.spec.jsx
+++ 03_Podstawy_narzedzi_do_testowania:_React_Testing_Library/01_Napisanie_pierwszych_testow_w_React_Testing_Library/src/TaskList/TaskList.spec.jsx
@@ -8,9 +8,16 @@ describe("<TaskList />", () => {
     render(<TaskList />);
     expect(screen.getByText("Data is loading...")).toBeInTheDocument();
   });
+
   test("updates tasks list after a delay", async () => {
     render(<TaskList />);
     const deleyedTasks = await screen.findByText("Task 1", "Task 2");
     expect(deleyedTasks).toBeInTheDocument();
+  });
+
+  test("initialiy does not show tasks list", () => {
+    render(<TaskList />);
+    const deleyedTaskList = screen.queryByText("Task 1", "Task 2");
+    expect(deleyedTaskList).not.toBeInTheDocument();
   });
 });
