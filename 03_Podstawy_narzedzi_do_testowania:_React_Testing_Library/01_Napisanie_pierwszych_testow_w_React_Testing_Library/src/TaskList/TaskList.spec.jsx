bdda0df848a15051d736650ea096d0302f7e2244
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-01 09:29:44 +0200

    added  test for updated task list after delay

--- 03_Podstawy_narzedzi_do_testowania:_React_Testing_Library/01_Napisanie_pierwszych_testow_w_React_Testing_Library/src/TaskList/TaskList.spec.jsx
+++ 03_Podstawy_narzedzi_do_testowania:_React_Testing_Library/01_Napisanie_pierwszych_testow_w_React_Testing_Library/src/TaskList/TaskList.spec.jsx
@@ -8,4 +8,9 @@ describe("<TaskList />", () => {
     render(<TaskList />);
     expect(screen.getByText("Data is loading...")).toBeInTheDocument();
   });
+  test("updates tasks list after a delay", async () => {
+    render(<TaskList />);
+    const deleyedTasks = await screen.findByText("Task 1", "Task 2");
+    expect(deleyedTasks).toBeInTheDocument();
+  });
 });
