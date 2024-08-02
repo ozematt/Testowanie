d552d1d111386cbc9f0a96c11d962755f46c293a
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-02 12:02:03 +0200

    solved exercise

--- 06_Testowanie_propsow_komponentu/src/UserProfile.spec.jsx
+++ 06_Testowanie_propsow_komponentu/src/UserProfile.spec.jsx
@@ -12,11 +12,27 @@ describe("<UserProfile /> test", () => {
   test("displays user avatar", () => {
     render(<UserProfile avatarUrl="www.example.com" />);
 
-    expect(screen.getByAltText("User avatar")).toBeInTheDocument();
+    expect(screen.getByAltText("User avatar")).toHaveAttribute(
+      "src",
+      "www.example.com"
+    );
   });
   test("not displays user avatar", () => {
     render(<UserProfile />);
 
     expect(screen.queryByAltText("User avatar")).not.toBeInTheDocument();
+  });
+  test("displays default text when there is no data", () => {
+    render(<UserProfile />);
+
+    expect(
+      screen.getByText("Name: Information not provided")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Surname: Information not provided")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Email: Information not provided")
+    ).toBeInTheDocument();
   });
 });
