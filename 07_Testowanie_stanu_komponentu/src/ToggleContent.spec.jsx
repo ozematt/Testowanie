22bad5cd98bc0d2b82eac9f81122a22448336906
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-05 11:42:05 +0200

    added component file and test file

---
+++ 07_Testowanie_stanu_komponentu/src/ToggleContent.spec.jsx
@@ -0,0 +1,6 @@
+import { screen, render } from "@testing-library/react";
+import { ToggleContent } from "./ToggleContent";
+
+describe("<ToggleContent />", () => {
+  test("content is not visible", () => {});
+});
