3980ac87ec46d03ebbe45df8b086a666f3143c81
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-05 11:59:58 +0200

    added gitignore files

--- 07_Testowanie_stanu_komponentu/src/ToggleContent.spec.jsx
+++ 07_Testowanie_stanu_komponentu/src/ToggleContent.spec.jsx
@@ -2,5 +2,8 @@ import { screen, render } from "@testing-library/react";
 import { ToggleContent } from "./ToggleContent";
 
 describe("<ToggleContent />", () => {
-  test("content is not visible", () => {});
+  test("content is hidden, is in the document ", () => {
+    render(<ToggleContent />);
+    expect(screen.getByText("Content is hidden")).toBeInTheDocument();
+  });
 });
