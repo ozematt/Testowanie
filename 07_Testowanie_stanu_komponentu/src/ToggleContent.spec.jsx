1388922dc3a0e3a96743f4dc45e7cedb2b71c7da
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-05 12:11:56 +0200

    added test

--- 07_Testowanie_stanu_komponentu/src/ToggleContent.spec.jsx
+++ 07_Testowanie_stanu_komponentu/src/ToggleContent.spec.jsx
@@ -1,9 +1,17 @@
 import { screen, render } from "@testing-library/react";
 import { ToggleContent } from "./ToggleContent";
+import { userEvent } from "@testing-library/user-event";
 
 describe("<ToggleContent />", () => {
   test("content is hidden, is in the document ", () => {
     render(<ToggleContent />);
     expect(screen.getByText("Content is hidden")).toBeInTheDocument();
+  });
+  test("content is visible", async () => {
+    render(<ToggleContent />);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    expect(await screen.findByText("Content is visible")).toBeInTheDocument();
   });
 });
