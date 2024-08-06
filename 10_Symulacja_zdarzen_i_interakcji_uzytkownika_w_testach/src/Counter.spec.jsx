131f658e9829ce91d063bdc6f63ffecd8593c9ef
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-06 12:54:11 +0200

    added test and some changes with initial state

--- 10_Symulacja_zdarzen_i_interakcji_uzytkownika_w_testach/src/Counter.spec.jsx
+++ 10_Symulacja_zdarzen_i_interakcji_uzytkownika_w_testach/src/Counter.spec.jsx
@@ -13,21 +13,21 @@ describe("<Counter />", () => {
     });
     test("checking if the initial value of the counter is the same as the value given in the initialValue prop (given in the range 0 - 10)", () => {
       const user = userEvent.setup();
-      render(<Counter initialValue="2" />);
+      render(<Counter initialValue={2} />);
       const count = screen.getByTestId("counter");
 
       expect(count).toHaveTextContent("2");
     });
     test("check if the initial value of the counter is 10 if the given initialization value is greater than 10", () => {
       const user = userEvent.setup();
-      render(<Counter initialValue="12" />);
+      render(<Counter initialValue={12} />);
       const count = screen.getByTestId("counter");
 
       expect(count).toHaveTextContent("10");
     });
     test("checking if the initial value of the counter is 0 if the given initialization value is less than 0", () => {
       const user = userEvent.setup();
-      render(<Counter initialValue="-12" />);
+      render(<Counter initialValue={-12} />);
       const count = screen.getByTestId("counter");
 
       expect(count).toHaveTextContent("0");
@@ -45,7 +45,7 @@ describe("<Counter />", () => {
     });
     test("checking whether clicking the button will decrease the counter by 1", async () => {
       const user = userEvent.setup();
-      render(<Counter initialValue="5" />);
+      render(<Counter initialValue={5} />);
       const count = screen.getByTestId("counter");
       const decrementButton = screen.getByRole("button", { name: "-" });
 
@@ -54,12 +54,24 @@ describe("<Counter />", () => {
     });
     test("check whether clicking the Reset button will reset the counter to zero", async () => {
       const user = userEvent.setup();
-      render(<Counter initialValue="5" />);
+      render(<Counter initialValue={5} />);
       const count = screen.getByTestId("counter");
       const resetButton = screen.getByRole("button", { name: "Reset" });
 
       await user.click(resetButton);
       expect(count).toHaveTextContent("0");
+    });
+  });
+  describe("Boundary condition test", () => {
+    test("checking whether, when the counter is at 10, clicking does not increase its status, and whether the button has a disabled status", async () => {
+      const user = userEvent.setup();
+      render(<Counter initialValue={10} />);
+      const count = screen.getByTestId("counter");
+      const incrementButton = screen.getByRole("button", { name: "+" });
+
+      await user.click(incrementButton);
+      expect(incrementButton).toBeDisabled();
+      expect(count).toHaveTextContent("10");
     });
   });
 });
