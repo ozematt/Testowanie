0e2208e5b7b62c335244c0da50d0fc8e211650e3
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-06 10:59:05 +0200

    added test2

--- 09_Strategie_testowania_komponentow/src/AdvancedInput.spec.jsx
+++ 09_Strategie_testowania_komponentow/src/AdvancedInput.spec.jsx
@@ -4,9 +4,20 @@ import { screen, render } from "@testing-library/react";
 describe("<AdvancedInput />", () => {
   test("Testing the presence of a component in a document", () => {
     render(<AdvancedInput isVisible />);
-
     const input = screen.getByRole("textbox");
 
     expect(input).toBeInTheDocument();
+  });
+  test("class presence testing", () => {
+    const { unmount } = render(<AdvancedInput isVisible className="active" />);
+    const input = screen.getByRole("textbox");
+
+    expect(input).toHaveClass("active");
+
+    unmount();
+
+    render(<AdvancedInput isVisible className="inactive" />);
+    const input1 = screen.getByRole("textbox");
+    expect(input1).toHaveClass("inactive");
   });
 });
