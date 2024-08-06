33b72bc1e1c07c751081a33336df9f4aec8cc391
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-06 16:02:08 +0200

    added files

---
+++ 11_Testowanie_stanu_hookow/src/useTextWithValidation.jsx
@@ -0,0 +1,13 @@
+export function useTextWithValidation(minLength = 3, maxLength = 20) {
+  const [text, setText] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChange = (newText) => {
+    const isValid = newText.length >= minLength && newText.length <= maxLength;
+
+    setIsValid(isValid);
+    setText(newText);
+  };
+
+  return { text, isValid, handleChange };
+}
