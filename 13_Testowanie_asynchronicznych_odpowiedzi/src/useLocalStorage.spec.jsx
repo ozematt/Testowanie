d0dfccb85d12c218d9126684c06bacd0a4d05a7a
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-07 10:48:44 +0200

    added test

--- 13_Testowanie_asynchronicznych_odpowiedzi/src/useLocalStorage.spec.jsx
+++ 13_Testowanie_asynchronicznych_odpowiedzi/src/useLocalStorage.spec.jsx
@@ -1,5 +1,5 @@
 import { useLocalStorage } from "./useLocalStorage";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 
 describe("useLocalStorage hook", () => {
   test("should initialize with the initial value when localStorage does not have the key", () => {
@@ -14,5 +14,16 @@ describe("useLocalStorage hook", () => {
     const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
 
     expect(result.current[0]).toBe("storedValue");
+  });
+  test("should be able to update the value", () => {
+    const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
+    const [, setValue] = result.current;
+
+    act(() => {
+      result.current[1]("newValue");
+    });
+
+    expect(result.current[0]).toBe("newValue");
+    expect(localStorage.getItem("key")).toBe(JSON.stringify("newValue"));
   });
 });
