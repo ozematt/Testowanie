59f5bcedec6f528143d1684a0235269f6f235ecc
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-07 12:48:24 +0200

    solved exercise

--- 13_Testowanie_asynchronicznych_odpowiedzi/src/RegistrationForm.spec.jsx
+++ 13_Testowanie_asynchronicznych_odpowiedzi/src/RegistrationForm.spec.jsx
@@ -1,5 +1,5 @@
 import { RegistrationForm } from "./RegistrationForm";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("<RegistrationForm />", () => {
@@ -40,5 +40,61 @@ describe("<RegistrationForm />", () => {
     expect(
       await screen.findByText("Provided email address is invalid.")
     ).toBeInTheDocument();
+  });
+  test("should display error when input password is empty", async () => {
+    const user = userEvent.setup();
+    render(<RegistrationForm />);
+
+    const email = screen.getByLabelText("Email:");
+    const password = screen.getByLabelText("Password:");
+    const registerButton = screen.getByRole("button", { name: "Register" });
+
+    await user.type(email, "example@gmail.com");
+    await user.click(registerButton);
+
+    expect(
+      await screen.findByText("Password is required.")
+    ).toBeInTheDocument();
+  });
+  test("should display error when password is to short", async () => {
+    const user = userEvent.setup();
+    render(<RegistrationForm />);
+
+    const email = screen.getByLabelText("Email:");
+    const password = screen.getByLabelText("Password:");
+    const registerButton = screen.getByRole("button", { name: "Register" });
+
+    await user.type(email, "example@gmail.com");
+    await user.type(password, "1234567");
+    await user.click(registerButton);
+
+    expect(
+      await screen.findByText("Password must be at least 8 characters long.")
+    ).toBeInTheDocument();
+  });
+  test("should display no error when form is fill correctly", async () => {
+    const user = userEvent.setup();
+    render(<RegistrationForm />);
+
+    const email = screen.getByLabelText("Email:");
+    const password = screen.getByLabelText("Password:");
+    const registerButton = screen.getByRole("button", { name: "Register" });
+
+    await user.type(email, "example@gmail.com");
+    await user.type(password, "123456789");
+    await user.click(registerButton);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Email is required.")).not.toBeInTheDocument();
+      expect(
+        screen.queryByText("Password is required.")
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText("Password must be at least 8 characters long.")
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText("Provided email address is invalid.")
+      ).not.toBeInTheDocument();
+    });
   });
 });
