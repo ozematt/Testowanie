f6b409712baec4db6ec2ed1f5c4bd031a0aa6933
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-07 12:36:12 +0200

    added next test

--- 13_Testowanie_asynchronicznych_odpowiedzi/src/RegistrationForm.spec.jsx
+++ 13_Testowanie_asynchronicznych_odpowiedzi/src/RegistrationForm.spec.jsx
@@ -1,15 +1,28 @@
 import { RegistrationForm } from "./RegistrationForm";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("<RegistrationForm />", () => {
   test("should render all form elements", () => {
     render(<RegistrationForm />);
-    const email = screen.getByRole("textbox", { id: "email" });
-    const password = screen.getByRole("textbox", { id: "password" });
+    const email = screen.getByLabelText("Email:");
+    const password = screen.getByLabelText("Password:");
     const registerButton = screen.getByRole("button", { name: "Register" });
 
     expect(email).toBeInTheDocument();
     expect(password).toBeInTheDocument();
     expect(registerButton).toBeInTheDocument();
+  });
+  test("should display error when input email is empty", async () => {
+    const user = userEvent.setup();
+    render(<RegistrationForm />);
+
+    const password = screen.getByLabelText("Password:");
+    const registerButton = screen.getByRole("button", { name: "Register" });
+
+    await user.type(password, "123456");
+    await user.click(registerButton);
+
+    expect(await screen.findByText("Email is required.")).toBeInTheDocument();
   });
 });
