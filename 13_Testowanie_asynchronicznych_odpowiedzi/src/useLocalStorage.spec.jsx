adee82fa9ce141c8fc2d727887c55bb48a02e8dd
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-07 11:27:29 +0200

    added files

--- 13_Testowanie_asynchronicznych_odpowiedzi/src/useLocalStorage.spec.jsx
+++ 13_Testowanie_asynchronicznych_odpowiedzi/src/useLocalStorage.spec.jsx
@@ -1,39 +1,39 @@
-import { useLocalStorage } from "./useLocalStorage";
-import { renderHook, act } from "@testing-library/react";
+// import { useLocalStorage } from "./useLocalStorage";
+// import { renderHook, act } from "@testing-library/react";
 
-describe("useLocalStorage hook", () => {
-  test("should initialize with the initial value when localStorage does not have the key", () => {
-    const { result } = renderHook(() =>
-      useLocalStorage("testKey", "defaultValue")
-    );
+// describe("useLocalStorage hook", () => {
+//   test("should initialize with the initial value when localStorage does not have the key", () => {
+//     const { result } = renderHook(() =>
+//       useLocalStorage("testKey", "defaultValue")
+//     );
 
-    expect(result.current[0]).toBe("defaultValue");
-  });
-  test("should initialize with the value from localStorage if it exists", () => {
-    localStorage.setItem("key", JSON.stringify("storedValue"));
-    const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
+//     expect(result.current[0]).toBe("defaultValue");
+//   });
+//   test("should initialize with the value from localStorage if it exists", () => {
+//     localStorage.setItem("key", JSON.stringify("storedValue"));
+//     const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
 
-    expect(result.current[0]).toBe("storedValue");
-  });
-  test("should be able to update the value", () => {
-    const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
-    const [, setValue] = result.current;
+//     expect(result.current[0]).toBe("storedValue");
+//   });
+//   test("should be able to update the value", () => {
+//     const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
+//     const [, setValue] = result.current;
 
-    act(() => {
-      result.current[1]("newValue");
-    });
+//     act(() => {
+//       result.current[1]("newValue");
+//     });
 
-    expect(result.current[0]).toBe("newValue");
-    expect(localStorage.getItem("key")).toBe(JSON.stringify("newValue"));
-  });
-  test("should update the value when localStorage changes", () => {
-    const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
+//     expect(result.current[0]).toBe("newValue");
+//     expect(localStorage.getItem("key")).toBe(JSON.stringify("newValue"));
+//   });
+//   test("should update the value when localStorage changes", () => {
+//     const { result } = renderHook(() => useLocalStorage("key", "initialValue"));
 
-    act(() => {
-      localStorage.setItem("key", JSON.stringify("externalValue"));
-      window.dispatchEvent(new Event("storage"));
-    });
+//     act(() => {
+//       localStorage.setItem("key", JSON.stringify("externalValue"));
+//       window.dispatchEvent(new Event("storage"));
+//     });
 
-    expect(result.current[0]).toBe("externalValue");
-  });
-});
+//     expect(result.current[0]).toBe("externalValue");
+//   });
+// });
