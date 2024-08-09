4ca5057f5e23be83aba68e7efffc3cb74e953250
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-09 10:09:53 +0200

    added tets

--- 15_Mockowanie_hookow/src/WeatherWidget.spec.jsx
+++ 15_Mockowanie_hookow/src/WeatherWidget.spec.jsx
@@ -1,9 +1,23 @@
+import { afterAll, expect } from "vitest";
 import { WeatherWidget } from "./WeatherWidget";
 import * as useWeatherFile from "./useWeather";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 const useWeatherSpy = vi.spyOn(useWeatherFile, "useWeather");
 
 const mockUseWeather = ({ data = {}, isLoading = false, isError = false }) => {
   useWeatherSpy.mockReturnValue({ data, isLoading, isError });
 };
+
+describe("<WeatherWidget />", () => {
+  afterAll(() => {
+    useWeatherSpy.mockRestore();
+  });
+  test("should display Loading", () => {
+    mockUseWeather({ isLoading: true });
+
+    render(<WeatherWidget />);
+
+    expect(screen.getByText("Loading weather data...")).toBeInTheDocument();
+  });
+});
