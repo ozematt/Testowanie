5431c4b499b5d2b0fce067dde6a8ceff3da8fb85
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-09 13:52:13 +0200

    added next test

--- 16_Testowanie_asynchronicznych_operacji/src/ProductDetails.spec.jsx
+++ 16_Testowanie_asynchronicznych_operacji/src/ProductDetails.spec.jsx
@@ -31,4 +31,12 @@ describe("<ProductDetails />", () => {
     expect(screen.getByText("Quantity: 5 products")).toBeInTheDocument();
     expect(screen.getByText("Rating: 9/10")).toBeInTheDocument();
   });
+  test("testing error message display", () => {
+    mockUseGetProductQuery({ isError: true });
+    render(<ProductDetails />);
+
+    expect(
+      screen.getByText("Could not load product details.")
+    ).toBeInTheDocument();
+  });
 });
