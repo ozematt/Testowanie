239a4f01256166f77aacbfe0f2c2d1f7092422b3
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-09 13:49:31 +0200

    added test

--- 16_Testowanie_asynchronicznych_operacji/src/ProductDetails.spec.jsx
+++ 16_Testowanie_asynchronicznych_operacji/src/ProductDetails.spec.jsx
@@ -1,6 +1,6 @@
 import { ProductDetails } from "./ProductDetails";
 import { screen, render } from "@testing-library/react";
-import { describe } from "vitest";
+import { afterAll, describe } from "vitest";
 import * as useGetProductQueryFile from "./api";
 
 const useGetProductQuerySpy = vi.spyOn(
@@ -13,11 +13,22 @@ const mockUseGetProductQuery = ({
   isError = false,
   isLoading = false,
 }) => {
-  useGetProductQuerySpy.mockReturnValue({ data: product, isError, isLoading });
+  useGetProductQuerySpy.mockReturnValue({ data, isError, isLoading });
 };
 
 describe("<ProductDetails />", () => {
+  afterAll(() => {
+    useGetProductQuerySpy.mockRestore();
+  });
   test("testing data display", () => {
+    mockUseGetProductQuery({
+      data: { id: 1, name: "TV", price: 120, quantity: 5, rating: "9" },
+    });
     render(<ProductDetails productId={1} />);
+
+    expect(screen.getByText("TV")).toBeInTheDocument();
+    expect(screen.getByText("Price: 120 EUR")).toBeInTheDocument();
+    expect(screen.getByText("Quantity: 5 products")).toBeInTheDocument();
+    expect(screen.getByText("Rating: 9/10")).toBeInTheDocument();
   });
 });
