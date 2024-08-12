2c3f70c5984ef7d08225ab9ebca2c868325f6d95
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-12 12:39:52 +0200

    edit setup.mjs

--- 16_Testowanie_asynchronicznych_operacji/setupTest.mjs
+++ 16_Testowanie_asynchronicznych_operacji/setupTest.mjs
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom/vitest";
+import { beforeAll, afterAll, afterEach } from "vitest";
+import { server } from "./src/_mocks_/server";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
