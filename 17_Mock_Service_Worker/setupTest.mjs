3f78010290c611ce34124e656c5817028fe2ef6a
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-12 11:55:52 +0200

    added setup

--- 17_Mock_Service_Worker/setupTest.mjs
+++ 17_Mock_Service_Worker/setupTest.mjs
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom/vitest";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./src/_mocks_/server";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
