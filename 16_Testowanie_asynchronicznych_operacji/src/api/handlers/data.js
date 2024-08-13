eeb1f44e11ae1407bc63a7a650ab033ad1949bb2
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-13 09:29:36 +0200

    added refactor handler

--- 16_Testowanie_asynchronicznych_operacji/src/api/handlers/data.js
+++ 16_Testowanie_asynchronicznych_operacji/src/api/handlers/data.js
@@ -1,20 +1,17 @@
-import { rest } from "msw";
+import { HttpResponse, http } from "msw";
 
 export const handlers = [
-  rest.get("https://www.example.com/api/products/:id", (req, res, ctx) => {
-    const { id } = req.params;
+  http.get("https://www.example.com/api/products/:id", ({ params }) => {
+    const { id } = params;
     if (id === "1") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          name: "iPhone 15 Pro Max",
-          price: 1949,
-          quantity: 102,
-          rating: 10,
-        })
-      );
+      return HttpResponse.json({
+        name: "iPhone 15 Pro Max",
+        price: 1949,
+        quantity: 102,
+        rating: 10,
+      });
     } else {
-      return res(ctx.status(404), ctx.json({ message: "Product not found" }));
+      return HttpResponse.json({ message: "Product not found" });
     }
   }),
 ];
